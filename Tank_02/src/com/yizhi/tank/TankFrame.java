cd38ec94c6bc021b3c172f016157f34275d8bcdb
yizhi <1027054698@qq.com>
2017-11-30 10:03:28 +0800

    Tank Project Edition02

---
+++ Tank_02/src/com/yizhi/tank/TankFrame.java
@@ -0,0 +1,11 @@
+package com.yizhi.tank;
+
+import java.awt.Frame;
+
+public class TankFrame {
+	public static void main(String[] args) {
+		Frame f = new Frame();
+		f.setSize(800,600);
+		f.setVisible(true);
+	}
+}
