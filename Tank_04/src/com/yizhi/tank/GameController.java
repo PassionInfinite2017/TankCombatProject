0ef83ac5785985131230cca508ae016c80c2e60b
yizhi <1027054698@qq.com>
2017-11-30 10:04:40 +0800

    Tank Project Edition04

---
+++ Tank_04/src/com/yizhi/tank/GameController.java
@@ -0,0 +1,9 @@
+package com.yizhi.tank;
+
+import java.awt.Frame;
+
+public class GameController {
+	public static void main(String[] args) {
+		Frame f = new TankFrame();
+	}
+}
---
+++ Tank_04/src/com/yizhi/tank/TankFrame.java
@@ -0,0 +1,11 @@
+package com.yizhi.tank;
+
+import java.awt.Frame;
+
+public class TankFrame  extends Frame{
+	public TankFrame(){
+		this.setSize(800,600);
+		this.setResizable(false);
+		this.setVisible(true);
+	}
+}
